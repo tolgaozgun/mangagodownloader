da5e5f703bbbac2c4cede90a85b5e07f89f9802c
Tolga Özgün <37017809+tolgaozgun@users.noreply.github.com>
2018-07-22 14:03:14 +0300

    Commit two: Chapter selection added

--- manga.rb
+++ manga.rb
@@ -3,18 +3,28 @@ require 'pry' # Ruby REPL
 require 'rb-readline' # Ruby IRB
 require 'awesome_print' # Console output
 require_relative 'credentials'
-
 require 'open-uri'
+
 manganame= $manganame
+chapter = $chapter
 filepath = $filepath
 browser = Watir::Browser.new :chrome
 browser.goto "mangago.me/read-manga/#{manganame}"
 
 browser.a(:text => "Start Reading").click()
 Watir::Wait.until { browser.windows.size == 2 }
-
 browser.window(:index => 1).use
 count = $frompage
+if chapter > 1
+  browser.a(:class => "chapter").click()
+  Watir::Wait.until{
+    browser.ul(:class => "chapter").visible?
+   }
+  chapterbutton = browser.ul(:class => "chapter").a(:index => chapter - 1)
+  chaptername = chapterbutton.text
+  chapterbutton.click()
+  sleep(1.0)
+end
   while !browser.img(:id => "page#{count}").exists? do
     browser.a(:text => "Pg 1").click()
     sleep(0.1)
@@ -22,12 +32,12 @@ count = $frompage
     sleep(1.0)
 end
 size = browser.ul(:id => "dropdown-menu-page").lis().size
-Dir.mkdir(filepath + manganame) unless File.exists?(filepath + manganame)
+Dir.mkdir(filepath + manganame + '/' + chaptername) unless File.exists?(filepath + manganame + "/" + chaptername)
 while count < size + 1 do
   filename = "page#{count}.png"
   image_src = browser.img(:class => "page#{count}").src
   clickthis = browser.img(:id => "page#{count}")
-  open(filepath + manganame + '/' + filename, 'wb') do |file|
+  open(filepath + manganame + '/' + chaptername + '/' + filename, 'wb') do |file|
 					file << open(image_src).read
 				end
         while !browser.img(:class => "page#{count + 2}").exists? do
